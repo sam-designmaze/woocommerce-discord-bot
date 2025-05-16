1ec3fc3eff02d5b57f1081bbf96ef7f1887181d2
sam-designmaze <sam@designmaze.us>
2025-05-17 02:42:11 +0500

    third

--- index.js
+++ index.js
@@ -88,3 +88,8 @@ client.on('interactionCreate', async interaction => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Bot is alive!'));
+app.listen(3000, () => console.log('Web server running on port 3000'));
